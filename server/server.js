2a59e6b6771114f836325753d3d3847b2f98d612
Moulik rai <raimoulik@gmail.com>
2024-01-30 11:33:19 +0545

    update: add cors in server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
+const cors = require("cors")
 const express = require('express')
 const { PORT } = process.env
 const app = express()
 
 const router = require("./router")
 
+app.use(cors())
+app.use(express.json())
 app.use('/', router)
 
 app.listen(PORT, () => {
