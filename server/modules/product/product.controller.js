0264ddb184a54d8bd60f2b0c088a2b67a43fa624
Moulik rai <raimoulik@gmail.com>
2024-01-30 11:33:41 +0545

    chore: generate base64 signature

--- server/modules/product/product.controller.js
+++ server/modules/product/product.controller.js
@@ -1,3 +1,17 @@
+const crypto = require("node:crypto")
+
+const { ESEWA_SECRET } = process.env
+
+ const generateSignature = message => {
+  const hash = crypto.createHmac("sha256", ESEWA_SECRET)
+  const hashInBase64 = hash.update(message).digest("base64")
+
+  return hashInBase64
+}
+
 exports.buyProduct = (req, res) => {
-  console.log('buy product')
+  const message = req.body.signedInput
+  const signature = generateSignature(message)
+
+  res.status(200).send({ success: true, signature })
 }
