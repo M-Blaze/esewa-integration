2abbc4587612d5940ca1fa863193b3290133f7d9
Moulik rai <raimoulik@gmail.com>
2024-02-01 13:17:54 +0545

    remove: product routes

--- server/modules/product/product.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-
-const productController = require('./product.controller')
-
-router.post('/buy', productController.buyProduct)
-
-module.exports = router
