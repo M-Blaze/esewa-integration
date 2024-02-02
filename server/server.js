368eeaa41c861a028948342c615b9cff12cfd162
Moulik rai <raimoulik@gmail.com>
2024-02-02 19:45:10 +0545

    update: server script to use "/api"

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
 const cors = require("cors")
 const express = require('express')
-const { PORT } = process.env
-const app = express()
 
+const app = express()
 const router = require("./router")
+const { PORT } = process.env
 
 app.use(cors())
 app.use(express.json())
-app.use('/', router)
+app.use('/api', router)
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`)
